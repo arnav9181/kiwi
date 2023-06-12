2856d48b5efdf69aef594a992a6b00f91663f8d8
jchu37 <71276938+jchu37@users.noreply.github.com>
2023-06-12 02:46:34 -0700

    Update CooldownBarController.cs

--- Assets/Scripts/CooldownBarController.cs
+++ Assets/Scripts/CooldownBarController.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class CooldownBarController : MonoBehaviour
 {
     public Image imageCooldown;
     public float cooldown = 5;
     bool isCooldown;
+    bool isHidden;
 
+    private float hideTime = 1.5f;
+    private float hideCooldownTime = 2f;
+    private float hideTimer;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q) && !isHidden)
         {
             isCooldown = true;
+            isHidden = true;
+            StartCoroutine(StartHideCooldown());
         }
-        if(isCooldown)
+
+        if (isCooldown)
         {
             imageCooldown.fillAmount += 1 / cooldown * Time.deltaTime;
 
@@ -28,5 +34,15 @@ public class CooldownBarController : MonoBehaviour
                 isCooldown = false;
             }
         }
+    }
+
+    IEnumerator StartHideCooldown()
+    {
+        yield return new WaitForSeconds(hideTime);
+
+        isHidden = false;
+        yield return new WaitForSeconds(hideCooldownTime);
+
+        isCooldown = true;
     }
 }
