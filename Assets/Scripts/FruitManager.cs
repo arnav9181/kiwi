eadafc61eb1f0b6d35906eac72e0399200564d24
liam <liammshaw@gmail.com>
2023-06-12 02:44:54 -0700

    finished writing checker

---
+++ Assets/Scripts/FruitManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitManager : MonoBehaviour
+{
+    //Apple = 0, Banana = 1, Cantalopue = 2, Grape = 3, Kale = 4, Melon = 5
+    public bool[] fruitCheck = new bool[] {false, false, false, false, false, false};
+    public int fruitCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
